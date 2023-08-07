e30aa71708ab4e25c1fcea3309a8331e37b04a72
Supriya Maddu <117802859+SupriyaMaddu@users.noreply.github.com>
2023-08-07 20:38:53 +0530

    Add files via upload

---
+++ stringOperations.py
@@ -0,0 +1,26 @@
+word = input()
+length = len(word)
+print(length)
+
+word = input()
+length = len(word)
+secret_msg = "*" * length
+print(secret_msg)
+
+word = input()
+length = len(word)
+last_index = length - 1 
+print(last_index)
+
+word = input()
+last_letter = len(word) - 1
+print(word[last_letter])
+
+word = input()
+length = len(word)
+print(word[0] + "*" * (length - 1))
+
+word = input()
+length = len(word)
+half_length = length/2
+print(half_length)
